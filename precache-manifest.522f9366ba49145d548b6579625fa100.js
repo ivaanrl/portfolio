ae6cfb3895622d0c5c6036e982d5543c7e07be2c
ivaanrl <ivanrl2010@gmail.com>
2020-02-06 15:38:09 -0300

    Updates

---
+++ precache-manifest.522f9366ba49145d548b6579625fa100.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a786601d5dc6e368c19cf9700b2342a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2120fa7d0fa518d08e1a",
+    "url": "/portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "ceba39f98f9e8659bbf2",
+    "url": "/portfolio/static/css/main.8870c4dc.chunk.css"
+  },
+  {
+    "revision": "2120fa7d0fa518d08e1a",
+    "url": "/portfolio/static/js/2.35d7cc53.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.35d7cc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceba39f98f9e8659bbf2",
+    "url": "/portfolio/static/js/main.513c1ce2.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "9a4aa935fa06d75f25700077d7321a8f",
+    "url": "/portfolio/static/media/Screenshot from 2020-02-06 14-56-12.9a4aa935.png"
+  },
+  {
+    "revision": "bfcd89439c27770d4b74e7c48ba2ca0d",
+    "url": "/portfolio/static/media/algorithmVisualizerScreenshot.bfcd8943.png"
+  },
+  {
+    "revision": "8061a4efaf9bd972b6759db9e067be2b",
+    "url": "/portfolio/static/media/twooterScreenshot.8061a4ef.png"
+  }
+]);
